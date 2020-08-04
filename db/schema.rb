98e76f18184d0337bd4e715babfa54d0321689c8
ldelbel <lucdelbel@gmail.com>
2020-08-04 11:52:22 -0300

    Migrate DB, create articles model, route and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_04_140423) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

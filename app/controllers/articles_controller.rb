cd3836d4694e2f7f8945ef2eff763fe66b138714
ldelbel <lucdelbel@gmail.com>
2020-08-05 06:22:20 -0300

    Add create article link t index temp, add show temp and styling file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,12 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @new_article = Article.new 
+  end
 end

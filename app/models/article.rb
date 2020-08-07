4da7fd25fbda82addffdbc5f6c1529f1f9d13c52
ElijahTobs <eligenius@gmail.com>
2020-08-07 09:19:09 +0100

    Add tags to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+  end
 end

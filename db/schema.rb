1020a1a58cccabcbef5aee854a7ea1eef7449bc6
ldelbel <lucdelbel@gmail.com>
2020-08-07 12:44:00 -0300

    Add view for tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_103354) do
+ActiveRecord::Schema.define(version: 2020_08_07_142124) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"

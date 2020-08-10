29ad43065361dae6c7f37865dca82a880714e9d5
ldelbel <lucdelbel@gmail.com>
2020-08-10 10:00:40 -0300

    Author Session files

---
+++ app/helpers/author_session_helper.rb
@@ -0,0 +1,2 @@
+module AuthorSessionHelper
+end
---
+++ test/controllers/author_session_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AuthorSessionControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
